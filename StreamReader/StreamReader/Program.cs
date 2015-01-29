a300a896b1f8d868ee983b715398a6f657781a39
chopta <chopta@gmail.com>
2015-01-29 10:40:03 -0800

    Daily change

--- StreamReader/StreamReader/Program.cs
+++ StreamReader/StreamReader/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace StreamReader
 {
@@ -14,7 +17,9 @@ namespace StreamReader
 			//CreateLargeFileWithEncoding();
 			//LoadLargeFile();
 			//PartialLoadLargeFile();
-			WhyUsingDispose();
+			//WhyUsingDispose();
+			//CreateLargeFileMb();
+			SocketMain();
 			//Stream vs reader vs File vs Buffer vs start, end, flush, 
 			//Socket
 			//Ping
@@ -69,31 +74,83 @@ namespace StreamReader
 
 		static void WhyUsingDispose()
 		{
-			var fileLocation = @"d:\largeFile\largeFile3.txt";
+			//for (var i = 0; i < 10; i++)
+			//{
+			//    var fileLocation = @"d:\largeFile\largeFile3.txt";
 
-			const int bufferSize = 20000;
-			var buffer = new char[bufferSize];
-			var count = bufferSize;
-			var fs = File.OpenRead(fileLocation);
-			var sr = new System.IO.StreamReader(fs, Encoding.UTF8);
+			//    const int bufferSize = 200000;
+			//    var buffer = new char[bufferSize];
+			//    var count = bufferSize;
+			//    var fs = File.OpenRead(fileLocation);
+			//    var sr = new System.IO.StreamReader(fs, Encoding.UTF8);
 
-			string value = "";
-			var sb = new StringBuilder(bufferSize);
-			while (count > 0)
+			//    string value = "";
+			//    var sb = new StringBuilder(bufferSize);
+			//    //while (count > 0)
+			//    //{
+			//    //    count = sr.Read(buffer, 0, bufferSize);
+			//    //    sb.Append(buffer, 0, count);
+			//    //    Console.WriteLine(sb.ToString());
+			//    //    sb.Clear();
+			//    //}
+
+
+
+			//}
+
+			for (var i = 0; i < 100; i++)
 			{
-				count = sr.Read(buffer, 0, bufferSize);
-				sb.Append(buffer, 0, count);
-				Console.WriteLine(sb.ToString());
-				sb.Clear();
+				var result = ReadAll();
 			}
-
-
 			Console.ReadKey();
 		}
 
-		static void CreateLargeFileIncrementally(int mb)
-		{
 
+		public static string ReadAll()
+		{
+			var filePath = @"d:\largeFile\largeFile2.txt";
+			var sr = new System.IO.StreamReader(filePath);
+			return sr.ReadToEnd();
+		}
+
+		public static string ReadAllDispose()
+		{
+			var filePath = @"d:\largeFile\largeFile2.txt";
+			using (var sr = new System.IO.StreamReader(filePath))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+
+		static void CreateLargeFileMb()
+		{
+			int mb = 0;
+			var input = Console.ReadLine();
+			if (int.TryParse(input, out mb))
+			{
+				var fileLocation = @"d:\largeFile\" + mb + "MbCreation.txt";
+				using (var ss = File.OpenWrite(fileLocation))
+				using (var sw = new StreamWriter(ss))
+				{
+					var kbbuilder = new StringBuilder(1024);
+					for (var i = 0; i < 1024; i++)
+					{
+						kbbuilder.Append("A");
+					}
+					var mbbuilder = new StringBuilder(1024, 1024 * 1000);
+					for (var i = 0; i < 1000; i++)
+					{
+						mbbuilder.Append(kbbuilder.ToString());
+					}
+					var resultbuilder = new StringBuilder(1024 * 1000 * mb);
+					for (var i = 0; i < mb; i++)
+					{
+						resultbuilder.Append(mbbuilder.ToString());
+					}
+					sw.Write(resultbuilder.ToString());
+				}
+			}
+			Console.ReadKey();
 		}
 
 		static void LoadLargeFile()
@@ -154,6 +211,98 @@ namespace StreamReader
 
 		static void DirectoryBrowsing()
 		{
+		}
+
+		static void MultiThreading()
+		{
+			
+		}
+
+		static void WebCrawler()
+		{
+			
+		}
+
+
+		private static Socket ConnectSocket(string server, int port)
+		{
+			Socket s = null;
+			IPHostEntry hostEntry = null;
+
+			// Get host related information.
+			hostEntry = Dns.GetHostEntry(server);
+
+			// Loop through the AddressList to obtain the supported AddressFamily. This is to avoid 
+			// an exception that occurs when the host IP Address is not compatible with the address family 
+			// (typical in the IPv6 case). 
+			foreach (IPAddress address in hostEntry.AddressList)
+			{
+				IPEndPoint ipe = new IPEndPoint(address, port);
+				Socket tempSocket =
+					new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+				tempSocket.Connect(ipe);
+
+				if (tempSocket.Connected)
+				{
+					s = tempSocket;
+					break;
+				}
+				else
+				{
+					continue;
+				}
+			}
+			return s;
+		}
+
+		// This method requests the home page content for the specified server. 
+		private static string SocketSendReceive(string server, int port)
+		{
+			string request = "GET / HTTP/1.1\r\nHost: " + server +
+				"\r\nConnection: Close\r\n\r\n";
+			Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+			Byte[] bytesReceived = new Byte[256];
+
+			// Create a socket connection with the specified server and port.
+			Socket s = ConnectSocket(server, port);
+
+			if (s == null)
+				return ("Connection failed");
+
+			// Send request to the server.
+			s.Send(bytesSent, bytesSent.Length, 0);
+
+			// Receive the server home page content. 
+			int bytes = 0;
+			string page = "Default HTML page on " + server + ":\r\n";
+
+			// The following will block until te page is transmitted. 
+			do
+			{
+				bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+				page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+			}
+			while (bytes > 0);
+
+			return page;
+		}
+
+		public static void SocketMain()
+		{
+			string host = "www.google.ca";
+			int port = 80;
+
+			//if (args.Length == 0)
+			//    // If no server name is passed as argument to this program,  
+			//    // use the current host name as the default.
+			//    host = "google.com";// Dns.GetHostName();
+			//else
+			//    host = args[0];
+
+			string result = SocketSendReceive(host, port);
+			Console.WriteLine(result);
+			Console.ReadKey();
 		}
 	}
 }
